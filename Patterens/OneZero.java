d2b98f02f1d94851214b25c2532932d57403ad64
Pratiksha Chitkute <Pallu@gmail.com>
2024-06-25 16:13:17 +0530

    Recursion folder

---
+++ Patterens/OneZero.java
@@ -0,0 +1,22 @@
+
+public class OneZero {
+
+    public static void main(String args[]) {
+        int n = 4, k = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                int sum = i + j;
+                if (sum % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
