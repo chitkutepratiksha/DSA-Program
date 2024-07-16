dcf277e5da57d5a7fa989cd974a7fd73cf7037e5
Pratiksha Chitkute <Pallu@gmail.com>
2024-07-16 20:05:26 +0530

    Function Folder

---
+++ Function/PrintName.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class PrintName {
+    public static void printName(String name) {
+        System.out.println(name);
+        return;
+
+    }
+
+    public static void main(String rags[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printName(name);
+
+    }
+
+}
---
+++ String/PrintString.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+public class PrintString {
+    public static void main(String args[]) {
+        String name = "Tony";
+        System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        String FirstName = sc.nextLine();
+        String Lastname = sc.nextLine();
+
+        // concatenation
+        String FullName = FirstName + " " + Lastname;
+        System.out.println(FullName);
+
+        // calculate length of string
+        System.out.println(FullName.length());
+
+        // dispay the character of string
+        for (int i = 0; i < FullName.length(); i++) {
+            System.out.println(FullName.charAt(i));
+        }
+
+        // compare two string using equals method
+        if (FirstName.equals(Lastname)) {
+            System.out.println("String are equal");
+        } else {
+            System.out.println("String are not equal");
+        }
+
+        // compare String using == operator
+        if (FirstName == Lastname) {
+            System.out.println("String are equal");
+        } else {
+            System.out.println("String are not Equal");
+        }
+
+        // compare String by creating new string
+        if (new String("Pratiksha") == new String("Pratiksha")) {
+            System.out.println("String are equal");
+        } else {
+            System.out.println("String are not Equal");
+        }
+
+        // substring
+
+        System.out.println(FirstName.substring(0, 6));
+        System.out.println(FirstName.substring(6));
+
+    }
+
+}
