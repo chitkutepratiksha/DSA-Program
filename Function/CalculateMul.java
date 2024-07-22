e2cd0d92a7a58293277717a50c1c7b1b3028a314
Pratiksha Rajendra Chitkute <109678977+chitkutepratiksha@users.noreply.github.com>
2024-07-22 14:24:51 +0530

    New file added

---
+++ Function/CalculateMul.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class CalculateMul {
+
+    public static int PrintMul(int a, int b) {
+        int mul = a * b;
+
+        return mul;
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("multiplication=" + PrintMul(a, b));
+
+        // int mul = PrintMul(a, b);
+        // System.out.println("Multiplication of two number = " + mul);
+
+    }
+
+}
