dc01f87439308da407a98586028efa645fd03818
Pratiksha Rajendra Chitkute <109678977+chitkutepratiksha@users.noreply.github.com>
2024-08-09 15:42:57 +0530

    Update BinaryToDecimal.java

--- Operators/BinaryToDecimal.java
+++ Operators/BinaryToDecimal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BinaryToDecimal{
+public class BinaryToDecimal {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
