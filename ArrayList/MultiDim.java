cb57d0f24d0f5a87a2b360fe3a0a4ce1d43c4ef5
Pratiksha Rajendra Chitkute <109678977+chitkutepratiksha@users.noreply.github.com>
2024-08-11 00:11:23 +0530

    Update MultiDim.java

--- ArrayList/MultiDim.java
+++ ArrayList/MultiDim.java
@@ -1,6 +1,6 @@
  import java.util.ArrayList;
 
-public class MultiDim {
+public class MultiDim   {
     public static void main(String args[]) {
         ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
         /*ArrayList<Integer> list1 = new ArrayList<>();
